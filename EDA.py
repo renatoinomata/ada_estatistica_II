e70f88321bd32a0cd4605e08deac05c829bf585d
renatoinomata <39674944+renatoinomata@users.noreply.github.com>
2023-05-31 20:43:45 -0300

    Adicionando páginas para cada análise

--- EDA.py
+++ EDA.py
@@ -3,88 +3,136 @@ import numpy as np
 import streamlit as st
 import matplotlib.pyplot as plt
 import seaborn as sns
-# plt.style.use('seaborn-v0_8-whitegrid')
-plt.style.use('default')
-# asdf
-st.title('Análise dos dados:')
+plt.style.use('seaborn-v0_8-whitegrid')
+# plt.style.use('default')
+
+st.title('Análise dos dados')
+st.write('Grupo 01: Claudia, Hevans, Voltolini, Renato, Vitor e William')
 
 # Importando dados
 df = pd.read_csv('./dataset/predictive_maintenance.csv')
 colunas = df.columns
 
-st.write('Conjunto de dados:')
-# Explicar variáveis?
+paginas = ['01 - Conjunto de dados', 
+           '02 - Contagem de dados', 
+           '03 - Distribuições', 
+           '04 - Correlações']
 
-# Data Frame
-st.write(df)
+df_failures = df[df.loc[:, 'Failure Type'] != 'No Failure']
 
-# Dados de df.info()
-st.write('Verificando as observações de cada coluna `df.info()`:')
+# Sidebar
+with st.sidebar:
+    pg_escolhida = st.selectbox('Página', paginas)
 
-df_info = pd.DataFrame({'Colunas': df.columns,
-                        'Observações não nulas': [len(df) for x in df.columns],
-                        'Nulos': [df[x].isnull().sum() for x in df.columns],
-                        'Dtype': [np.dtype(df[x]) for x in df.columns]})
-st.write(df_info)
+st.header(pg_escolhida)
+# Conjunto de dados
+if pg_escolhida == paginas[0]:
+    # Explicar variáveis?
 
-# Dados de df.describe()
-st.write('Principais métricas dos dados:')
-st.write(df.describe())
+    # Data Frame
+    st.write(df)
+
+    # Dados de df.info()
+    st.write('Verificando as observações de cada coluna `df.info()`:')
+
+    df_info = pd.DataFrame({'Colunas': df.columns,
+                            'Observações não nulas': [len(df) for x in df.columns],
+                            'Nulos': [df[x].isnull().sum() for x in df.columns],
+                            'Dtype': [np.dtype(df[x]) for x in df.columns]})
+    st.write(df_info)
+
+    # Dados de df.describe()
+    st.write('Principais métricas dos dados:')
+    st.write(df.describe())
 
 # Contagem com value_counts()
-st.write(df.value_counts('Type'))
-st.write(df.value_counts('Failure Type'))
-
-col_escolhida = st.selectbox('Escolha o parâmetro',
-                             [
-                                #  'UDI',
-                                #  'Product ID',
-                                #  'Type',
-                                 'Air temperature [K]', 
-                                 'Process temperature [K]',
-                                 'Rotational speed [rpm]', 
-                                 'Torque [Nm]',
-                                 'Tool wear [min]',
-                                #  'Target',
-                                # 'Failure Type',
-                                 ])
-with st.container():
+if pg_escolhida == paginas[1]:
     
-    fig = plt.figure(constrained_layout = True, figsize=(16,9))
-    gs = fig.add_gridspec(1, 2)
+    var1 = st.selectbox('Contagem dos valores da variável:', ['Type', 'Target', 'Failure Type'])
+    # st.write('Contagem dos valores da variável:')
+    col1, col2 = st.columns([2, 1])
+    with col1:
+        if var1 == 'Failure Type':
+            sns.countplot(df_failures, y = var1)
+        else:
+            sns.countplot(df, y = var1)
+        plt.title('Contagem por categoria da variável')
+        plt.xlabel('Contagem')
+        st.pyplot(plt.gcf())
+    with col2:
+        st.write(df.value_counts(var1))
 
-    ax1 = fig.add_subplot(gs[0, 0])
-    sns.histplot(df, x = col_escolhida, bins = 'auto', hue = 'Type')
-    ax1.set_title(f'Histograma da variável `{col_escolhida}` por tipo de máquina')
+    if st.button('Spoilers'):
+        st.write('Como vimos, existe algum tipo de inconsistência nas falhas, vamos descobrir qual o erro:')
+        st.write(df.value_counts(['Target', 'Failure Type']))
 
-    ax2 = fig.add_subplot(gs[0, 1])
-    sns.boxplot(df, y = 'Type', x = col_escolhida, orient = 'h')
-    ax2.set_title(f'Boxplot da variável `{col_escolhida}` por tipo de máquina')
+# Distribuições
+if pg_escolhida == paginas[2]:
+    col_escolhida = st.selectbox('Histogramas e boxplots da variável',
+                                [
+                                    #  'UDI',
+                                    #  'Product ID',
+                                    #  'Type',
+                                    'Air temperature [K]', 
+                                    'Process temperature [K]',
+                                    'Rotational speed [rpm]', 
+                                    'Torque [Nm]',
+                                    'Tool wear [min]',
+                                    #  'Target',
+                                    # 'Failure Type',
+                                    ])
+    with st.container():
+        
+        st.subheader('Gráficos por tipo de máquina')
 
-    st.pyplot(fig)
+        fig = plt.figure(constrained_layout = True, figsize=(16,9))
+        gs = fig.add_gridspec(1, 2)
 
-# Construção dos histogramas e boxplots por variáveis
-df_failures = df[df.loc[:, 'Failure Type'] != 'No Failure']
-with st.container():
+        ax1 = fig.add_subplot(gs[0, 0])
+        sns.histplot(df, x = col_escolhida, bins = 'auto', hue = 'Type')
+        ax1.set_title(f'Histograma da variável `{col_escolhida}` por tipo de máquina')
 
-    fig = plt.figure(constrained_layout = True, figsize=(16,9))
-    gs = fig.add_gridspec(2, 2)
+        ax2 = fig.add_subplot(gs[0, 1])
+        sns.boxplot(df, y = 'Type', x = col_escolhida, orient = 'h')
+        ax2.set_title(f'Boxplot da variável `{col_escolhida}` por tipo de máquina')
 
-    ax1 = fig.add_subplot(gs[0, 0])
-    sns.histplot(df, x = col_escolhida, bins = 'auto', hue = 'Target')
-    ax1.set_title(f'Histograma da variável `{col_escolhida}` por ocorrência de falha')
+        st.pyplot(fig)
+
+    # Construção dos histogramas e boxplots por variáveis
+ 
+    with st.container():
+
+        st.subheader('Gráficos por falhas e tipos de falhas')
+        
+        fig = plt.figure(constrained_layout = True, figsize=(16,9))
+        gs = fig.add_gridspec(2, 2)
+
+        ax1 = fig.add_subplot(gs[0, 0])
+        sns.histplot(df, x = col_escolhida, bins = 'auto', hue = 'Target')
+        ax1.set_title(f'Histograma da variável `{col_escolhida}` por ocorrência de falha')
 
 
-    ax2 = fig.add_subplot(gs[0, 1])
-    sns.histplot(df_failures, x = col_escolhida, hue = 'Failure Type')
-    ax2.set_title(f'Histograma da variável `{col_escolhida}` por tipo de falha')
+        ax2 = fig.add_subplot(gs[0, 1])
+        sns.histplot(df_failures, x = col_escolhida, hue = 'Failure Type')
+        ax2.set_title(f'Histograma da variável `{col_escolhida}` por tipo de falha')
 
-    ax3 = fig.add_subplot(gs[1, 0])
-    sns.boxplot(df, y = 'Target', x = col_escolhida, orient = 'h')
-    ax3.set_title(f'Boxplot da variável `{col_escolhida}` por ocorrência de falha')
+        ax3 = fig.add_subplot(gs[1, 0])
+        sns.boxplot(df, y = 'Target', x = col_escolhida, orient = 'h')
+        ax3.set_title(f'Boxplot da variável `{col_escolhida}` por ocorrência de falha')
 
-    ax4 = fig.add_subplot(gs[1, 1])
-    sns.boxplot(df_failures, y = 'Failure Type', x = col_escolhida, orient = "h")
-    ax4.set_title(f'Boxplot da variável `{col_escolhida}` por tipo de falha')
+        ax4 = fig.add_subplot(gs[1, 1])
+        sns.boxplot(df_failures, y = 'Failure Type', x = col_escolhida, orient = "h")
+        ax4.set_title(f'Boxplot da variável `{col_escolhida}` por tipo de falha')
 
-    st.pyplot(fig)
+        st.pyplot(fig)
+
+# Correlações
+if pg_escolhida == paginas[3]:
+    st.write('Correlações')
+    matriz_corr = df.select_dtypes(include = np.number).drop(columns = 'UDI').corr()
+    st.write(matriz_corr)
+    if st.button('Spoilers'):
+        sns.heatmap(matriz_corr, annot = True, cmap = 'coolwarm')
+        st.pyplot(plt.gcf())
+ 
+    
